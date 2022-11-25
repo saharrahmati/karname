b43051b2297adf0f72591becbd9d131c10055d93
s.rahmati <sahar.rahmati60@gmail.com>
2022-11-25 16:18:58 +0330

    created details and subject components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/Navbar';
+import Questions from './components/Questions';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Questions />
     </div>
   );
 }
---
+++ src/components/Questions/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './index.module.css'
+
+function Questions () {
+    return(
+        <div className={classes.QuestionsList}>
+            <div>
+                
+            </div>
+            <div>
+                 هراد دوجو یلکشم هچ اقیقد دینیبب هک متشاذگ مه ور console یجورخ سکع .تساجک زا لکشم منودیمن .هدیم مهب ور error نیا اما مزاسب react وت هداس authentication هی ماوخیم نم مالس
+            </div>
+        </div>
+    )
+}
+
+export default Questions
