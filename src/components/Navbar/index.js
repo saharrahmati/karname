6e4f986a100e0fc4ee2e3061b90acf434b0dc74f
s.rahmati <sahar.rahmati60@gmail.com>
2022-11-25 15:42:04 +0330

    added style to NavBar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import classes from './index.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
 
 function NavBar() {
     return (
         <div className={classes.NavBar}>
             <h1>لیست سوالات </h1>
             <div className={classes.NavInfo}>
-                <div>سوالات جدید</div>
-                <div>سحر رحمتی</div>
+                <button className={classes.AddQuestion}>
+                    <FontAwesomeIcon icon={faPlus} />
+                    <span>سوالات جدید</span>
+                </button>
+                <div className={classes.UserInfo}>
+                    <FontAwesomeIcon icon={faUserCircle} className={classes.UserPic}/>
+                    <span>سحر رحمتی</span>
+                    <FontAwesomeIcon icon={faCaretDown} />
+                </div>
             </div>
         </div>
     )
